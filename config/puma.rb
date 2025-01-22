32d43809fa1d7ba9fcda5145d4a8470209a58879
Henrique <h.rodrigues@runtime-revolution.com>
2025-01-22 16:15:38 +0000

    Fixing issue with puma

--- config/puma.rb
+++ config/puma.rb
@@ -30,7 +30,7 @@ threads threads_count, threads_count
 
 preload_app!
 
-rackup      DefaultRackup
+rackup      DefaultRackup if defined?(DefaultRackup)
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port        ENV["PORT"]     || 3000
 environment ENV["RACK_ENV"] || "development"
